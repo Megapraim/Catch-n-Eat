fe69b5f42efb2a0498e4c723d8d0921d6b9216e4
Megapraim <megapraim@gmail.com>
2015-09-02 21:35:08 +0300

    Update game.js
    Yeah! It's our first big update in gameplay! Now you don't need to refresh page every time you want to play another time.

--- game.js
+++ game.js
@@ -12,6 +12,10 @@ var UPPERCASE_W =119;
 var LOWERCASE_S =83;
 var UPPERCASE_S =115;
 
+function boardStart() { //Saving the start board
+	startBoard = document.getElementById("board").innerHTML;
+}
+
 function checkNumber(player, button, direction, key1, key2) {
 	if ((button.keyCode == key1) || (button.keyCode == key2)) { 
 	//For player 1 we need only keyCode1 (ignoring keyCode2), second keyCode is only for player 2 (because there are different codes for d and D, w and W, etc.)
@@ -21,6 +25,7 @@ function checkNumber(player, button, direction, key1, key2) {
 			play.removeAttribute("class");
 			var newLocations = document.getElementById(locations);
 			newLocations.setAttribute("class", player);
+			isEndOfGame();
 		}
 	}	
 }
@@ -57,4 +62,25 @@ function onBoard(locations) {
 	return true;
 }
 
+function isEndOfGame() { //Check, did anybody win?
+	var pl1 = document.getElementsByClassName("player1")[0];
+	var pl2 = document.getElementsByClassName("player2")[0];
+	if (pl1 == null) {
+		showWin("Player 2");
+	} else if (pl2 == null) {
+		showWin("Player 1");
+	}
+}
+
+function showWin(winner) { //Display message with congrutalations and start the game again
+	var board = document.getElementById("board");
+	var winId;
+	if (winner == "Player 1") {winId = "winner1"} else {winId = "winner2"}
+	board.innerHTML = "<tr> <td id=\"winMessage\"> <h1 id="+ winId +">" + winner + " wins!</h1> <br/> <h2>--Wait 3 seconds before next match--</h2> </td> </tr>";
+	var startLevel = function() {document.getElementById("board").innerHTML = startBoard;};
+	setTimeout(startLevel, 3000);
+}
+
 window.onkeydown = move;
+var startBoard;
+window.onload = boardStart;
