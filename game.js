c5ae24b760b9f86c5c6a0081e1aa814f85c5d5d1
Megapraim <megapraim@gmail.com>
2015-09-07 20:24:09 +0300

    Update game.js

--- game.js
+++ game.js
@@ -12,75 +12,89 @@ var UPPERCASE_W =119;
 var LOWERCASE_S =83;
 var UPPERCASE_S =115;
 
-function boardStart() { //Saving the start board
-	startBoard = document.getElementById("board").innerHTML;
+var view = {
+	showWin: function(winner) { //Display message with congrutalations and start the game again
+				var board = document.getElementById("board");
+				var winId;
+				if (winner == "Player 1") {winId = "winner1"; this.showScore(1);} else {winId = "winner2"; this.showScore(0);}
+				board.innerHTML = "<tr> <td id=\"winMessage\"> <h1 id="+ winId +">" + winner + " wins!</h1> <br/> <h2>--Wait 2 seconds before next match--</h2> </td> </tr>";
+				var startLevel = function() {document.getElementById("board").innerHTML = startBoard;};
+				setTimeout(startLevel, 2000);
+	},
+	showScore: function(winner) { 
+		if (winner == 1) {document.getElementById("score1").innerHTML = control.player1Score;} else
+		{document.getElementById("score2").innerHTML = control.player2Score;}
+	}
 }
 
-function checkNumber(player, button, direction, key1, key2) {
-	if ((button.keyCode == key1) || (button.keyCode == key2)) { 
-	//For player 1 we need only keyCode1 (ignoring keyCode2), second keyCode is only for player 2 (because there are different codes for d and D, w and W, etc.)
-		var play = document.getElementsByClassName(player)[0];
-		var locations = directions(direction, play);
-		if (onBoard(locations)) {
-			play.removeAttribute("class");
-			var newLocations = document.getElementById(locations);
-			newLocations.setAttribute("class", player);
-			isEndOfGame();
+var control = {
+	player1Score: 0,
+	player2Score: 0,
+	checkButton: function(player, button, direction, key1, key2) {
+					if ((button.keyCode == key1) || (button.keyCode == key2)) { 
+						//For player 1 we need only keyCode1 (ignoring keyCode2), second keyCode is only for player 2 (because there are 	different codes for d and D, w and W, etc.)
+						var play = document.getElementsByClassName(player)[0];
+						var locations = model.directions(direction, play);
+						if (model.onBoard(locations)) {
+							play.removeAttribute("class");
+							var newLocations = document.getElementById(locations);
+							newLocations.setAttribute("class", player);
+							model.isEndOfGame();
+						}
+					}	
+	}
+}
+
+var model = {	
+	boardStart: function() { //Saving the start board
+		startBoard = document.getElementById("board").innerHTML;
+	},
+
+	directions: function(direction, player) { //Generates locations for different directions
+		var locations = player.id;
+		if (direction == "right") {return (locations[0] + (locations[1] - (-1) ) ) }
+		if (direction == "left") {return (locations[0] + (locations[1] - 1 ) ) }
+		if (direction == "up") {return ((locations[0] - 1) + locations[1] ) }
+		if (direction == "down") {return ((locations[0] - (-1)) + locations[1] ) }
+	},
+
+	move: function(button) {
+		//Player 1 moves
+		control.checkButton("player1", button, "right", RIGHT_ARROW);
+		control.checkButton("player1", button, "left", LEFT_ARROW);
+		control.checkButton("player1", button, "up", UP_ARROW);
+		control.checkButton("player1", button, "down", DOWN_ARROW);
+		//Player 2 moves
+		control.checkButton("player2", button, "right", LOWERCASE_D, UPPERCASE_D);
+		control.checkButton("player2", button, "left", LOWERCASE_A, UPPERCASE_A);
+		control.checkButton("player2", button, "up", LOWERCASE_W, UPPERCASE_W);
+		control.checkButton("player2", button, "down", LOWERCASE_S, UPPERCASE_S);
+	},
+
+	//Check, is it on the board?
+	onBoard: function(locations) {
+		if ((locations[0] == 0) || (locations[1] == 0)) {
+			return false;
 		}
-	}	
-}
+		if (locations.length > 2) {
+			return false;
+		}
+		return true;
+	},
 
-function directions(direction, player) { //Generates locations for different directions
-	var locations = player.id;
-	if (direction == "right") {return (locations[0] + (locations[1] - (-1) ) ) }
-	if (direction == "left") {return (locations[0] + (locations[1] - 1 ) ) }
-	if (direction == "up") {return ((locations[0] - 1) + locations[1] ) }
-	if (direction == "down") {return ((locations[0] - (-1)) + locations[1] ) }
-}
-
-function move(button) {
-	//Player 1 moves
-	checkNumber("player1", button, "right", RIGHT_ARROW);
-	checkNumber("player1", button, "left", LEFT_ARROW);
-	checkNumber("player1", button, "up", UP_ARROW);
-	checkNumber("player1", button, "down", DOWN_ARROW);
-	//Player 2 moves
-	checkNumber("player2", button, "right", LOWERCASE_D, UPPERCASE_D);
-	checkNumber("player2", button, "left", LOWERCASE_A, UPPERCASE_A);
-	checkNumber("player2", button, "up", LOWERCASE_W, UPPERCASE_W);
-	checkNumber("player2", button, "down", LOWERCASE_S, UPPERCASE_S);
-}
-
-//Check, is it on the board?
-function onBoard(locations) {
-	if ((locations[0] == 0) || (locations[1] == 0)) {
-		return false;
-	}
-	if (locations.length > 2) {
-		return false;
-	}
-	return true;
-}
-
-function isEndOfGame() { //Check, did anybody win?
-	var pl1 = document.getElementsByClassName("player1")[0];
-	var pl2 = document.getElementsByClassName("player2")[0];
-	if (pl1 == null) {
-		showWin("Player 2");
-	} else if (pl2 == null) {
-		showWin("Player 1");
+	isEndOfGame: function() { //Check, did anybody win?
+		var pl1 = document.getElementsByClassName("player1")[0];
+		var pl2 = document.getElementsByClassName("player2")[0];
+		if (pl1 == null) {
+			control.player2Score++;
+			view.showWin("Player 2");
+		} else if (pl2 == null) {
+			control.player1Score++;
+			view.showWin("Player 1");
+		}
 	}
 }
 
-function showWin(winner) { //Display message with congrutalations and start the game again
-	var board = document.getElementById("board");
-	var winId;
-	if (winner == "Player 1") {winId = "winner1"} else {winId = "winner2"}
-	board.innerHTML = "<tr> <td id=\"winMessage\"> <h1 id="+ winId +">" + winner + " wins!</h1> <br/> <h2>--Wait 3 seconds before next match--</h2> </td> </tr>";
-	var startLevel = function() {document.getElementById("board").innerHTML = startBoard;};
-	setTimeout(startLevel, 3000);
-}
-
-window.onkeydown = move;
+window.onkeydown = model.move;
 var startBoard;
-window.onload = boardStart;
+window.onload = model.boardStart;
