7ae6de86b23f07c17f795a57ecf2d35fd9767b1a
Megapraim <megapraim@gmail.com>
2016-08-17 15:20:02 +0300

    Update game.js
    Now we can change sizes of the gameboard! Also we can control the start position of every player

--- game.js
+++ game.js
@@ -17,8 +17,8 @@ var view = {
 				var board = document.getElementById("board");
 				var winId;
 				if (winner == "Player 1") {winId = "winner1"; this.showScore(1);} else {winId = "winner2"; this.showScore(0);}
-				board.innerHTML = "<tr> <td id=\"winMessage\"> <h1 id="+ winId +">" + winner + " wins!</h1> <br/> <h2>--Wait 2 seconds before next match--</h2> </td> </tr>";
-				var startLevel = function() {document.getElementById("board").innerHTML = startBoard;};
+				board.innerHTML = "<tr> <td id=\"winMessage\"> <h1 id="+ winId +">" + winner + " scores!</h1> <br/> <h2>--Wait 2 seconds before next round--</h2> </td> </tr>";
+				var startLevel = function() {document.getElementById("board").innerHTML = model.board;};
 				setTimeout(startLevel, 2000);
 	},
 	showScore: function(winner) { 
@@ -32,7 +32,7 @@ var control = {
 	player2Score: 0,
 	checkButton: function(player, button, direction, key1, key2) {
 					if ((button.keyCode == key1) || (button.keyCode == key2)) { 
-						//For player 1 we need only keyCode1 (ignoring keyCode2), second keyCode is only for player 2 (because there are 	different codes for d and D, w and W, etc.)
+						//For player 1 we need only keyCode1 (ignoring keyCode2), second keyCode is only for player 2 (because there are different codes for d and D, w and W, etc.)
 						var play = document.getElementsByClassName(player)[0];
 						var locations = model.directions(direction, play);
 						if (model.onBoard(locations)) {
@@ -46,16 +46,36 @@ var control = {
 }
 
 var model = {	
-	boardStart: function() { //Saving the start board
-		startBoard = document.getElementById("board").innerHTML;
+	width: 9,
+	height: 9,
+	player1Start: "9-9",
+	player2Start: "1-1",
+	board: "",
+
+	boardStart: function() { //Creating the start board
+		var startBoard = document.getElementById("board");
+		this.board = "";
+		for (var i = 1; i <= this.height; i++) {
+			this.board += "<tr>"
+			for (var j = 1; j <= this.width ; j++) {
+				this.board = this.board + '<td id="' + i + '-' + j + '">';
+			}
+			this.board += "</tr>";
+		}
+		startBoard.innerHTML = this.board;
+		var start1 = document.getElementById(this.player1Start);
+		start1.setAttribute("class", "player1");
+		var start2 = document.getElementById(this.player2Start);
+		start2.setAttribute("class", "player2");
+		this.board = startBoard.innerHTML;
 	},
 
 	directions: function(direction, player) { //Generates locations for different directions
-		var locations = player.id;
-		if (direction == "right") {return (locations[0] + (locations[1] - (-1) ) ) }
-		if (direction == "left") {return (locations[0] + (locations[1] - 1 ) ) }
-		if (direction == "up") {return ((locations[0] - 1) + locations[1] ) }
-		if (direction == "down") {return ((locations[0] - (-1)) + locations[1] ) }
+		var locations = player.id.split("-");
+		if (direction == "right") {return (locations[0] + "-" + (locations[1] - (-1) ) ) }
+		if (direction == "left") {return (locations[0] + "-" + (locations[1] - 1 ) ) }
+		if (direction == "up") {return ((locations[0] - 1) + "-" + locations[1] ) }
+		if (direction == "down") {return ((locations[0] - (-1)) + "-" + locations[1] ) }
 	},
 
 	move: function(button) {
@@ -73,10 +93,8 @@ var model = {
 
 	//Check, is it on the board?
 	onBoard: function(locations) {
-		if ((locations[0] == 0) || (locations[1] == 0)) {
-			return false;
-		}
-		if (locations.length > 2) {
+		var locations = locations.split("-");
+		if ((locations[0] <= 0) || (locations[1] <= 0) || (locations[0] > this.height) || (locations[1] > this.width) ) {
 			return false;
 		}
 		return true;
@@ -96,5 +114,4 @@ var model = {
 }
 
 window.onkeydown = model.move;
-var startBoard;
-window.onload = model.boardStart;
+window.onload = function() {model.boardStart(); }
